b3c38c9e61beb182d01f09d92f345ef3f50f2b08
robertkartaviy-ops <robertkartaviy@gmail.com>
2025-12-08 09:57:05 +0300

    burger-menu

--- script.js
+++ script.js
@@ -46,3 +46,23 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const closeMenu = document.getElementById('closeMenu')
+
+    hamburger.addEventListener('click', (e) => {
+        mobileMenu.classList.add('active');
+    })
+
+    closeMenu.addEventListener('click', (e) => {
+        e.preventDefault();
+        mobileMenu.classList.remove('active'); 
+    })
+
+    document.addEventListener('click', (e) => {
+        if (!mobileMenu.contains(e.target) && !hamburger.contains(e.target)) {
+            mobileMenu.classList.remove('active');
+        }
+    })
+})
